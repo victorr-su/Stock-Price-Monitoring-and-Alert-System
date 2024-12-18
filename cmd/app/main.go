6f2b40a289dc002ad9ea91b2d156fe00b43c74d6
Victor <su.victor21@gmail.com>
2024-12-18 17:34:11 -0500

    Initial Commit

---
+++ cmd/app/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Stock price application begins");
+	// go kafka.StartProducer()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module Stock-Price-Monitoring-and-Alert-System
+
+go 1.21.6
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
